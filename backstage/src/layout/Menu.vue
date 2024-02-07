b59397914e74a02214e4310ada75424f684e9800
John Doe <309865580@qq.com>
2024-02-08 00:29:38 +0800

    init project

---
+++ backstage/src/layout/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+    <ElMenuItem>控制台</ElMenuItem>
+    <ElSubMenu>
+        <template #title>
+            权限管理
+        </template>
+        <ElMenuItem>管理员</ElMenuItem>
+        <ElMenuItem>系统菜单</ElMenuItem>
+        <ElMenuItem>角色</ElMenuItem>
+    </ElSubMenu>
+</template>
---
+++ backstage/src/layout/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+    import Menu  from "@/layout/Menu.vue";
+    
+</script>
+<template>
+    <ElContainer>
+        <ElAside width="220px" router  defeault-active="0">
+            <ElMenu>
+                <Menu></Menu>
+            </ElMenu>   
+       </ElAside>
+       <ElContainer>
+            <ElHeader>
+                this is of header 
+            </ElHeader>
+            <ElMain>
+                fsdfsf
+                <RouterView></RouterView>
+            </ElMain>
+       </ElContainer>
+    </ElContainer>
+</template>
+<style lang="scss" scope>
+   .layout{
+        
+   }
+</style>
